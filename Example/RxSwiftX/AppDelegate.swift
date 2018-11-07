d587716007596d95d57fd9dc53a70645f2675611
Pircate <gao497868860@163.com>
2018-11-07 23:20:48 +0800

    Update to swift 4.2

--- Example/RxSwiftX/AppDelegate.swift
+++ Example/RxSwiftX/AppDelegate.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
+#if swift(>=4.2)
+typealias ApplicationLaunchOptionsKey = UIApplication.LaunchOptionsKey
+#else
+typealias ApplicationLaunchOptionsKey = UIApplicationLaunchOptionsKey
+#endif
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [ApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
